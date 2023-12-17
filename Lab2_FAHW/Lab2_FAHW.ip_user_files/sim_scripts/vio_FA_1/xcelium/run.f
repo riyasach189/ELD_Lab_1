53ce1d8ac47cc7bdc7b10001239237c74ba19800
Riya Sachdeva <riyasach189@gmail.com>
2023-12-17 18:10:24 +0530

    removed trash

--- Lab2_FAHW/Lab2_FAHW.ip_user_files/sim_scripts/vio_FA_1/xcelium/run.f
+++
@@ -1,7 +0,0 @@
--makelib xcelium_lib/xil_defaultlib \
-  "../../../../Lab2_FAHW.srcs/sources_1/ip/vio_FA_1/sim/vio_FA.v" \
--endlib
--makelib xcelium_lib/xil_defaultlib \
-  glbl.v
--endlib
-
--- Lab2_FAHW/Lab2_FAHW.ip_user_files/sim_scripts/vio_subtractor/xcelium/run.f
+++
@@ -1,7 +0,0 @@
--makelib xcelium_lib/xil_defaultlib \
-  "../../../../Lab2_FAHW.srcs/sources_1/ip/vio_subtractor/sim/vio_subtractor.v" \
--endlib
--makelib xcelium_lib/xil_defaultlib \
-  glbl.v
--endlib
-
